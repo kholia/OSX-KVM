bdbc4061df5a5bc64e861f26fea2f547f8230fe0
Nicholas Sherlock <n.sherlock@gmail.com>
2018-06-17 21:04:00 +1200

    Initial support for Mojave Developer Preview

---
+++ Mojave/README.md
@@ -0,0 +1,9 @@
+### Notes
+
+Mojave installs the same way as High Sierra. The only difference is that Mojave now requires 
+CPU instructions that were introduced with Nehalem, so you will need to add CPU feature
+flags for ssse3, sse4.2, and popcnt to avoid Illegal Instruction crashes in the graphics
+subsystem after boot is complete (causing the top menu bar to flash on and off, and Finder
+to crash on open). 
+
+Tested with Clover 4542.
---
+++ Mojave/create_iso_mojave.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Bail at first ISO creation error
+set -e
+
+# Borrrowed from multiple internet sources
+hdiutil create -o ~/Desktop/Mojave.cdr -size 5800m -layout SPUD -fs HFS+J
+hdiutil attach ~/Desktop/Mojave.cdr.dmg -noverify -mountpoint /Volumes/install_build
+sudo /Applications/Install\ macOS\ 10.14\ Beta.app/Contents/Resources/createinstallmedia --volume /Volumes/install_build --nointeraction
+hdiutil detach "/Volumes/Install macOS 10.14 Beta"
+hdiutil convert ~/Desktop/Mojave.cdr.dmg -format UDTO -o ~/Desktop/Mojave.iso
+mv ~/Desktop/Mojave.iso.cdr ~/Desktop/Mojave.iso
+rm ~/Desktop/Mojave.cdr.dmg
