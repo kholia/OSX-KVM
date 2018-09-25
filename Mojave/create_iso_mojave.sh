a8f16518c0188096fb9dc3b83a0ef700fd081d77
Dhiru Kholia <dhiru.kholia@gmail.com>
2018-09-25 09:18:13 +0530

    Batch Update for September, 2018

--- Mojave/create_iso_mojave.sh
+++ Mojave/create_iso_mojave.sh
@@ -7,7 +7,7 @@ set -e
 hdiutil create -o ~/Desktop/Mojave.cdr -size 6g -layout SPUD -fs HFS+J
 hdiutil attach ~/Desktop/Mojave.cdr.dmg -noverify -mountpoint /Volumes/install_build
 sudo /Applications/Install\ macOS\ Mojave\ Beta.app/Contents/Resources/createinstallmedia --volume /Volumes/install_build --nointeraction
-hdiutil detach "/Volumes/Install macOS Mojave Beta"
+hdiutil detach "/Volumes/Install macOS Mojave"
 hdiutil convert ~/Desktop/Mojave.cdr.dmg -format UDTO -o ~/Desktop/Mojave.iso
 mv ~/Desktop/Mojave.iso.cdr ~/Desktop/Mojave.iso
 rm ~/Desktop/Mojave.cdr.dmg
