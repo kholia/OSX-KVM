6cb3ec71db863ab722e4568fd25517cd918c33e8
dcemmerson <emmersod@oregonstate.edu>
2020-10-14 12:33:16 -0700

    Update boot-passthrough.sh to be more inline with OpenCore-Boot.sh. Updated file references.

--- boot-passthrough.sh
+++ boot-passthrough.sh
@@ -10,25 +10,26 @@ OVMF_DIR="."
 
 # shellcheck disable=SC2054
 args=(
-  -enable-kvm -m "$ALLOCATED_RAM" -cpu host,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on
-  -machine pc-q35-2.9
+  -enable-kvm -m "$ALLOCATED_RAM" -cpu Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,"$MY_OPTIONS"
+  -machine q35
   -smp "$CPU_THREADS",cores="$CPU_CORES",sockets="$CPU_SOCKETS"
-  -vga none
-  -device pcie-root-port,bus=pcie.0,multifunction=on,port=1,chassis=1,id=port.1
-  -device vfio-pci,host=01:00.0,bus=port.1,multifunction=on
-  -device vfio-pci,host=01:00.1,bus=port.1
-  -usb -device usb-kbd -device usb-tablet
+  -device usb-ehci,id=ehci
+  -device usb-kbd,bus=ehci.0
+  -device usb-mouse,bus=ehci.0
+  -device nec-usb-xhci,id=xhci
   -device isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
-  -drive if=pflash,format=raw,readonly,file=OVMF_CODE.fd
-  -drive if=pflash,format=raw,file=OVMF_VARS.fd
+  -drive if=pflash,format=raw,readonly,file="$REPO_PATH/$OVMF_DIR/OVMF_CODE.fd"
+  -drive if=pflash,format=raw,file="$REPO_PATH/$OVMF_DIR/OVMF_VARS-1024x768.fd"
   -smbios type=2
-  -drive id=MacHDD,if=none,file=./mac_hdd.img
-  -device ide-drive,bus=sata.2,drive=MacHDD
+  -device ich9-intel-hda -device hda-duplex
+  -device ich9-ahci,id=sata
+  -drive id=MacHDD,if=none,file="$REPO_PATH/mac_hdd_ng.img",format=qcow2
+  -device ide-hd,bus=sata.2,drive=MacHDD
   -drive id=OpenCoreBoot,if=none,snapshot=on,format=qcow2,file="$REPO_PATH/OpenCore-Catalina/OpenCore-Passthrough.qcow2"
   -device ide-hd,bus=sata.3,drive=OpenCoreBoot
   -netdev tap,id=net0,ifname=tap0,script=no,downscript=no -device vmxnet3,netdev=net0,id=net0,mac=52:54:00:c9:18:27
   -monitor stdio
-  -display none
+  -vga vmware
 )
 
 qemu-system-x86_64 "${args[@]}"
