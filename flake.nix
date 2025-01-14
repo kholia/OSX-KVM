f0e3c1be5c361e5b4d56d8d9388713a713b4aee2
Pavel Shirshov <pshirshov@gmail.com>
2025-01-14 14:51:46 +0000

    flake.nix and direnv

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/24.05";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    ,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs.buildPackages; [
+            python3
+            dmg2img
+            qemu_kvm
+          ];
+        };
+      }
+    );
+}
+
